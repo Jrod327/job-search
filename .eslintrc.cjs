d3e679b0e50ca57cf9e47bd1f510495bcef84f74
Jrod327 <113052919+Jrod327@users.noreply.github.com>
2023-01-01 18:10:25 -0500

    Progress on Nav Bar and more config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,9 +7,12 @@ module.exports = {
     "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "@vue/eslint-config-prettier",
+    "plugin:vitest-globals/recommended",
   ],
   parserOptions: {
     ecmaVersion: "latest",
   },
-  endOfLine: "off",
+  env: {
+    "vitest-globals/env": true,
+  },
 };
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,9 +1,15 @@
 <template>
   <header class="w-full text-sm">
-    <div class="fixed top-0 left-0 w-full h-16 bg-white">
-      <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8 mx-auto">
-        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
-        <h2 class="ml-8 flex h-full items-center">Developed by {{ author }}</h2>
+    <div class="fixed top-0 left-0 h-16 w-full bg-white">
+      <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
+        <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li v-for="navItem in navItems" :key="navItem" class="ml-9 h-full first:ml-0">
+              <a href="" class="flex h-full items-center py-2.5">{{ navItem }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -15,9 +21,16 @@ export default {
   data() {
     return {
       company: "Dev Careers",
-      author: "Jared Henry",
-      
-    }
+      url: "https://careers.google.com",
+      navItems: [
+        "Teams",
+        "Locations",
+        "Life at Dev Careers",
+        "How We Hire",
+        "Students",
+        "Jobs",
+      ],
+    };
   },
-}
+};
 </script>
