e87a144f43d219daee78e1fba2d9cb5e8702f658
Jrod327 <113052919+Jrod327@users.noreply.github.com>
2023-01-03 21:52:57 -0500

    Dummy login feature + make login button dynamic via props

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,23 +1,44 @@
 <template>
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 h-16 w-full bg-white">
-      <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
-        <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
+      <div
+        class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
+      >
+        <a :href="url" class="flex h-full items-center text-xl">{{
+          company
+        }}</a>
         <nav class="ml-12 h-full">
           <ul class="flex h-full list-none">
-            <li v-for="navItem in navItems" :key="navItem" class="ml-9 h-full first:ml-0">
-              <a href="" class="flex h-full items-center py-2.5">{{ navItem }}</a>
+            <li
+              v-for="navItem in navItems"
+              :key="navItem"
+              class="ml-9 h-full first:ml-0"
+            >
+              <a href="" class="flex h-full items-center py-2.5">{{
+                navItem
+              }}</a>
             </li>
           </ul>
         </nav>
+        <div class="ml-auto flex h-full items-center">
+          <profile-image v-if="isLoggedIn" @click="toggleLogin" />
+          <action-button v-else text="Sign In" @click="toggleLogin" />
+        </div>
       </div>
     </div>
   </header>
 </template>
 
 <script>
+import ActionButton from "@/components/ActionButton.vue";
+import ProfileImage from "@/components/ProfileImage.vue";
+
 export default {
   name: "MainNav",
+  components: {
+    ActionButton,
+    ProfileImage,
+  },
   data() {
     return {
       company: "Dev Careers",
@@ -30,7 +51,13 @@ export default {
         "Students",
         "Jobs",
       ],
+      isLoggedIn: false,
     };
+  },
+  methods: {
+    toggleLogin() {
+      this.isLoggedIn = !this.isLoggedIn;
+    },
   },
 };
 </script>
