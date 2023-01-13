4b54fff2c2603d4934d162b92d7075f270c17b1b
Jrod327 <113052919+Jrod327@users.noreply.github.com>
2023-01-12 22:16:59 -0500

    init router

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -6,12 +6,7 @@
     <div class="flex h-full flex-1 flex-nowrap text-base font-light">
       <div class="relative flex h-full flex-1 items-center pr-3">
         <label class="absolute left-0 -top-10">Role</label>
-        <input
-          v-model="role"
-          type="text"
-          placeholder="Software Engineer"
-          class="w-full text-lg font-normal focus:outline-none"
-        />
+        <text-input v-model="role" placeholder="Software Engineer" />
       </div>
       <span
         class="flex h-full items-center border-l border-r border-brand-gray-3 bg-brand-gray-2 px-3"
@@ -20,12 +15,7 @@
       </span>
       <div class="relative flex h-full flex-1 items-center pl-3">
         <label class="absolute left-4 -top-10">Where?</label>
-        <input
-          v-model="location"
-          type="text"
-          placeholder="Los Angeles"
-          class="w-11/12 text-lg font-normal focus:outline-none"
-        />
+        <text-input v-model="location" placeholder="Los Angeles" />
       </div>
     </div>
     <action-button text="Search" type="secondary" class="rounded-r-3xl" />
@@ -34,10 +24,14 @@
 
 <script>
 import ActionButton from "@/components/Shared/ActionButton.vue";
+import TextInput from "@/components/Shared/TextInput.vue";
 
 export default {
   name: "JobSearchForm",
-  components: { ActionButton },
+  components: {
+    ActionButton,
+    TextInput,
+  },
   data() {
     return {
       role: "",
--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { faHooli } from "@fortawesome/free-brands-svg-icons";
 
+import router from "@/router";
 import App from "./App.vue";
 import "@/index.css";
 
 library.add(faSearch, faHooli);
 
-createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
+createApp(App)
+  .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
